11e73e6fd76d80fe5da278245d7db1b13ad8d91a
Riccardo Giorato <giorat.96@gmail.com>
2020-06-20 08:39:08 +0200

    new short motto for the company

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,9 @@ import React, { Fragment } from "react"
 const HomeIndex = () => {
   return (
     <Fragment>
-      <img className="db cf w-100 mw6 center" alt="talekeep logo" src="/assets/talekeep.png" />
+      <img className="db cf w-90 mw6 center pa2" alt="talekeep logo" src="/assets/talekeep.png" />
       <h3 className="tc pb3 f3">
-        The creative studio that builds virtual worlds from the real world
+      We scan the real world to create virtual worlds with you - 3D Scans as a Service 
       </h3>
       <section className="cf w-100 pa2-ns mw7 center">
         <div className="sketchfab-embed-wrapper">
